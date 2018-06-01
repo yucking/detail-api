568ef44d6c82154e25329d25fb815649b12314a5
Hang Zhang <8041160+zhanghang1989@users.noreply.github.com>
2018-06-01 10:03:28 -0700

    Update __init__.py

--- PythonAPI/detail/__init__.py
+++ PythonAPI/detail/__init__.py
@@ -98,10 +98,10 @@ class Detail:
             self.occlusion[occl['image_id']] = occl
         for bound in self.data['annos_boundary']:           # one per image
             self.bounds[bound['image_id']] = bound
-        for skeleton in self.data['annos_joints']:          # many per image
-            # skeletons are 1-indexed in JSON file and
-            # 0-indexed in self.kpts
-            self.kpts[skeleton['person_id'] - 1] = skeleton
+        #for skeleton in self.data['annos_joints']:          # many per image
+        #    # skeletons are 1-indexed in JSON file and
+        #    # 0-indexed in self.kpts
+        #    self.kpts[skeleton['person_id'] - 1] = skeleton
 
 
         # Follow references
